26487ee665dfc027255242ab2fe68a3485f7e533
SHANNTHINI <SHANNTHINI@GMAIL.COM>
2019-01-23 23:10:37 -0800

    "Add new item" bar button added to the list page.

--- KaroNa/View/ToDoListViewController.swift
+++ KaroNa/View/ToDoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
 
-    let itemArray = ["buy kadugu", "Teach Shruthika to ride a bike", "plan spring trip"]
+    var itemArray = ["buy kadugu", "Teach Shruthika to ride a bike", "plan spring trip"]
     
     
     override func viewDidLoad() {
@@ -51,10 +51,27 @@ class ToDoListViewController: UITableViewController {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
         
-        
-        
     }
+    
+    //Mark - add new items to the list
 
-
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "Add new Item to the list", message: "", preferredStyle: .alert)
+        var textField = UITextField()
+        
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
