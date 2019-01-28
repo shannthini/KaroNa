c791e208271052cf4db0ecdc135fd403a0e8292a
SHANNTHINI <SHANNTHINI@GMAIL.COM>
2019-01-27 19:12:09 -0800

    Used UserDefaults to store data locally

--- KaroNa/View/ToDoListViewController.swift
+++ KaroNa/View/ToDoListViewController.swift
@@ -11,11 +11,16 @@ import UIKit
 class ToDoListViewController: UITableViewController {
 
     var itemArray = ["buy kadugu", "Teach Shruthika to ride a bike", "plan spring trip"]
+    let defaults = UserDefaults.standard
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if let items = defaults.array(forKey: "ItemListArray") as? [String] {
+            itemArray = items
+        }
+        
         
     }
 
@@ -61,6 +66,7 @@ class ToDoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             self.itemArray.append(textField.text!)
+            self.defaults.set(self.itemArray, forKey: "ItemListArray")
             self.tableView.reloadData()
         }
         
